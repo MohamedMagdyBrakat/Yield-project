b1206e8d153f64c14f56101fb3d4369925221db3
Mohamed Barakat <mohamed.barakat@gs1eg.org>
2024-05-08 19:39:14 +0300

    Add project files.

---
+++ Yield project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace Yield_project;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        Console.WriteLine("\n\nusing for loop");
+        for (int i = 0; i < numbers.Count; i++)
+            Console.Write(numbers[i]+"\t");
+
+        Console.WriteLine("\n\nusing foreach");
+        foreach (int number in numbers)
+            Console.Write(number+"\t");
+
+        //complier dosen't understand foreach , it translate it to IL intermediate languge to the beyond example 
+        Console.WriteLine("\n\nusing custom foreach");
+        Foreach(numbers);
+        
+    }
+    static void Foreach<T>(IEnumerable<T> source)
+    {
+        IEnumerator<T> enumerator = source.GetEnumerator();
+        try
+        {
+            while (enumerator.MoveNext())
+            {
+                T item;
+                item = enumerator.Current;
+                Console.Write(item);
+            }
+        }
+        finally
+        {
+            enumerator.Dispose();
+        }
+    }
+}
---
+++ Yield/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Yield
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("\n\nGenerateV1()");
+            //foreach (int number in GenerateV1())
+            //    Console.Write($"  {number}  ");
+
+            //Console.WriteLine("\n\nGenerateV2()");
+            //foreach (int number in GenerateV2())
+            //    Console.Write($"  {number}  ");
+
+            //yield return an item for the caller each time 
+            Console.WriteLine("\n\nGenerateV3()");
+            foreach (int number in GenerateV3())
+                Console.Write($"  {number}  ");
+        }
+        static IEnumerable<int> GenerateV1()
+        {
+            List<int> result = [];
+            for (int i = 1; i <= 5; i++)
+                result.Add(i);
+            return result;
+        }
+        static IEnumerable<int> GenerateV2()
+        {
+            for (int i = 1; i <= 5; i++)
+                yield return i;
+        }
+
+        static IEnumerable<int> GenerateV3()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+            yield return 6;
+            yield break;
+            yield return 7;
+            yield return 8;
+            yield return 9;
+        }
+    }
+}
